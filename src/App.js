5be371f5af58f69ed03f1ea64d80d69ceed34a41
Arshad991 <nizamiarshad02@gmail.com>
2023-10-18 12:06:18 +0530

    files added

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import './styles/App.css'
+import Leftside from './components/Leftside'
+import Form from './components/Form'
+import Cardback from './components/Cardback'
+import Cardfront from './components/Cardfront'
+
+ 
+function App() { 
+  const [data, setData] = useState('')
+
+ 
+  return (
+    <div className="App">
+       <Leftside></Leftside>
+       <Form setData = {setData} ></Form>
+       <div className="cards">
+        <Cardfront card = {data} ></Cardfront>
+        <Cardback card = {data} ></Cardback>
+       </div>
+    </div>
+  )
+} 
+
+export default App
